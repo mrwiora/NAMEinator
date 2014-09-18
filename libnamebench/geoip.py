0e4b833e144937a766edd7119ed7b02eda981c7e
Matthias R. Wiora <matthias@wiora.co.uk>
2014-09-18 18:44:34 +0200

    geoip.py - Google Loc JSON API and MaxmindJSAPI have been shutdown - replacing by freegeoip.net

--- libnamebench/geoip.py
+++ libnamebench/geoip.py
@@ -24,53 +24,29 @@ import simplejson
 import util
 
 
-def GetFromGoogleLocAPI():
-  """Use the Google Loc JSON API from Google Gears.
-
-  Returns:
-    A dictionary containing geolocation information
-
-  NOTE: This is in violation of the Gears Terms of Service. See:
-  http://code.google.com/p/gears/wiki/GeolocationAPI
-  """
-  h = httplib2.Http(tempfile.gettempdir(), timeout=10)
-  url = 'http://www.google.com/loc/json'
-  post_data = {'request_address': 'true', 'version': '1.1.0', 'source': 'namebench'}
-  unused_resp, content = h.request(url, 'POST', simplejson.dumps(post_data))
-  try:
-    data = simplejson.loads(content)['location']
-    return {
-        'region_name': data['address'].get('region'),
-        'country_name': data['address'].get('country'),
-        'country_code': data['address'].get('country_code'),
-        'city': data['address'].get('city'),
-        'latitude': data['latitude'],
-        'longitude': data['longitude'],
-        'source': 'gloc'
+def GetFromFreegeoip():
+    apiUrl = 'http://www.freegeoip.net/json'
+    h = httplib2.Http()
+    response, content = h.request(apiUrl, 'GET')
+    results = json.loads(content)
+    output = ''
+    order = {
+        'country': 1,
+        'city': 2,
+        'region_name': 3,
+        'region_code': 4,
+        'longitude': 5,
+        'latitude': 6,
+        'ip': 7,
     }
-  except:
-    print '* Failed to use GoogleLocAPI: %s (content: %s)' % (util.GetLastExceptionString(), content)
-    return {}
-
-
-def GetFromMaxmindJSAPI():
-  h = httplib2.Http(tempfile.gettempdir(), timeout=10)
-  unused_resp, content = h.request('http://j.maxmind.com/app/geoip.js', 'GET')
-  keep = ['region_name', 'country_name', 'city', 'latitude', 'longitude', 'country_code']
-  results = dict([x for x in re.findall("geoip_(.*?)\(.*?\'(.*?)\'", content) if x[0] in keep])
-  results.update({'source': 'mmind'})
-  if results:
     return results
-  else:
-    return {}
-
 
 def GetGeoData():
   """Get geodata from any means necessary. Sanitize as necessary."""
   try:
-    json_data = GetFromGoogleLocAPI()
-    if not json_data:
-      json_data = GetFromMaxmindJSAPI()
+    json_data = GetFromFreegeoip()
+    # if not json_data:
+      # json_data = GetFromGoogleLocAPI()
 
     # Make our data less accurate. We don't need any more than that.
     json_data['latitude'] = '%.3f' % float(json_data['latitude'])
